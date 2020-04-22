bfed0e9b80a2488f48bc574f22c27d149100285f
Maraboom <maraboom13@gmail.com>
2020-04-22 17:22:00 +0300

    добавлены класс для корзины товара, класс для элемента массива, методы для подсчета суммы всех элементов и элементов корзины

--- script.js
+++ script.js
@@ -30,6 +30,51 @@ class GoodsList {
     });
     document.querySelector(".goods-list").innerHTML = listHtml;
   }
+  //метод, определяющий суммарную стоимость всех товаров
+  getPrice() {
+    let summ = 0;
+    this.goods.forEach((good) => {
+      summ += good.price;
+    });
+  }
+}
+//класс для элемента корзины
+class BasketItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="basket-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+//класс для корзины товаров
+class BasketList {
+  constructor() {
+    this.basket = [];
+  }
+  //метод для заполнения козины элементами, пока фиксированные элементы
+  fetchBasket() {
+    this.basket = [
+      { title: "Shirt", price: 150 },
+      { title: "Shoes", price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = "";
+    this.goods.forEach((good) => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector(".basket-list").innerHTML = listHtml;
+  }
+  //метод для подсчета суммы элементов корзины
+  getPrice() {
+    let summ = 0;
+    this.basket.forEach((good) => {
+      summ += good.price;
+    });
+  }
 }
 const list = new GoodsList();
 list.fetchGoods();
