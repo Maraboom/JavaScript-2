6f1a76268b0aa9b15d085013cfb9ddc29754fd78
Maraboom <maraboom13@gmail.com>
2020-04-29 01:03:06 +0300

    Сделано задание с кавычками

---
+++ JS4.js
@@ -0,0 +1,9 @@
+"use strict";
+const str =
+  "Fjhhfhchc 'fjsgfgshfs' fvdgdgdgd aren't dvhsgsh 'cshf' aren't 'vfdv' xbbbdg";
+
+const regexp = /^'|(\s)'|'(\s)|'$/g;
+
+console.log(str);
+
+console.log(str.replace(regexp, '$1"$2'));
