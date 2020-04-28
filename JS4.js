eb00755fb7e94f9248a82e5bd79a8ed6f4ae8104
Maraboom <maraboom13@gmail.com>
2020-04-29 01:26:18 +0300

    Создана форма без валидации полей

--- JS4.js
+++ JS4.js
@@ -1,9 +1,9 @@
-"use strict";
-const str =
-  "Fjhhfhchc 'fjsgfgshfs' fvdgdgdgd aren't dvhsgsh 'cshf' aren't 'vfdv' xbbbdg";
+// "use strict";
+// const str =
+//   "Fjhhfhchc 'fjsgfgshfs' fvdgdgdgd aren't dvhsgsh 'cshf' aren't 'vfdv' xbbbdg";
 
-const regexp = /^'|(\s)'|'(\s)|'$/g;
+// const regexp = /^'|(\s)'|'(\s)|'$/g;
 
-console.log(str);
+// console.log(str);
 
-console.log(str.replace(regexp, '$1"$2'));
+// console.log(str.replace(regexp, '$1"$2'));
